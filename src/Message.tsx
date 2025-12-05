7397d1ab306f565927e537974575ec3e79c29cb0
Cameron Robinson <robinson.cameron90@gmail.com>
2025-12-05 11:56:13 +0100

    Create ListGroup component

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,9 +1,8 @@
-function Message(){
-  return (
-    <h1 className="message">
-      Hello world
-    </h1>
-  );
+function Message() {
+  const name = "Cam";
+
+  if (name) <h1 className="message">return Hello {name}!</h1>;
+  return <h1 className="message">Hello world!</h1>;
 }
 
 export default Message;
