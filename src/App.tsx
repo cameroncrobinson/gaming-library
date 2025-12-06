d46fd0385cd32ee500738a5cf1453052ae1de3c6
Cameron Robinson <robinson.cameron90@gmail.com>
2025-12-06 23:09:43 +0100

    Button ui functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import ListGroup from "./components/ListGroup";
+import Alerts from "./components/Alerts";
+import Button from "./components/Button";
 
 function App() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Berlin"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
   return (
     <div className="App">
-      <ListGroup items={items} heading="Ipsum" />
+      <Alerts>
+        <span>Alert message</span>
+      </Alerts>
+      <Button>Click me</Button>
     </div>
   );
 }
